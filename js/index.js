54215cce55dc365182541b6cbcf79bce48e2dcd4
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2023-10-02 06:42:58 +0200

    added music

--- js/index.js
+++ js/index.js
@@ -1,6 +1,44 @@
-window.onload = function () {
-    // Получите элемент audio по его id
-    const music = document.getElementById("start-music");
-    // Воспроизведите музыку с помощью метода play
-    music.play();
+'use strict'
+console.log("Код загружен");
+
+
+const musicBlockLoader = document.querySelector('.music_block__loader');
+console.log(musicBlockLoader);
+const musicBlockStroke = document.querySelectorAll('.music_block__stroke');
+console.log(musicBlockStroke);
+const music = document.querySelector('#start-music');
+console.log(music);
+
+music.onloadeddata = function () {
+
+};
+
+musicBlockLoader.onclick = () => {
+    toggleMusic(music)
+}
+
+
+
+
+/* Запус музыки на сайте */
+function toggleMusic(soundTrack) {
+    checkMusic(soundTrack);
+    function checkMusic(currentMusic) {
+        if (check(currentMusic)) {
+            music.pause()
+            console.log('Is Paused');
+            for (const iterator of musicBlockStroke) {
+                iterator.classList.remove('music_is_play')
+            }
+        } else {
+            console.log('Is Play');
+            music.play();
+            for (const iterator of musicBlockStroke) {
+                iterator.classList.add('music_is_play')
+            }
+        }
+    };
+    function check(check) {
+        return !check.paused
+    };
 }
