6f191258d43a93a8887d0178f8c6b8148a20b5dd
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2023-10-02 03:16:03 +0200

    test music

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    // Получите элемент audio по его id
+    const music = document.getElementById("start-music");
+    // Воспроизведите музыку с помощью метода play
+    music.play();
+}
